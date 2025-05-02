3d460492d6b69127acba139f2111f8d84487909d
emmanuel-x123 <esongz456@gmail.com>
2025-05-02 10:55:53 -0700

    made changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/IQbase/', // Must match your repo name exactly
-  plugins: [react()],
+  root: './',
   build: {
-    outDir: 'docs',       // GitHub Pages needs either 'docs' or gh-pages branch
-    emptyOutDir: true,    // Clear folder before rebuild
-    assetsDir: 'assets',  // Organize assets in /assets
-  }
+    outDir: 'dist',
+  },
 });
